8883d560e605f7d8f0203eda49376869642700ae
kirinnee <kirinnee97@gmail.com>
2019-05-06 13:33:05 +0800

    [Feat] Change CDN and S3 forwarding

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -2,7 +2,7 @@ import {RuleSetQuery} from "webpack";
 
 let prodFileOpts: RuleSetQuery = {
 	name: '[path][name].[hash].[ext]',
-	publicPath: 'https://d3iwbuomikd272.cloudfront.net/',
+	publicPath: 'https://d3t35au9pm0ra5.cloudfront.net/',
 	context: './src'
 };
 
