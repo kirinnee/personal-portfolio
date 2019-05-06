30b02162a626732a9696a3b7c5c52c5f63e22bc0
kirinnee <kirinnee97@gmail.com>
2019-05-06 12:52:22 +0800

    [Feat]Update CDN and image configuration

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -2,7 +2,7 @@ import {RuleSetQuery} from "webpack";
 
 let prodFileOpts: RuleSetQuery = {
 	name: '[path][name].[hash].[ext]',
-	publicPath: 'https://d1w2hran25a477.cloudfront.net/',
+	publicPath: 'https://d3iwbuomikd272.cloudfront.net/',
 	context: './src'
 };
 
